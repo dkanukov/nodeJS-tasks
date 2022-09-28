b5fd199aafee20ac3a9c356359542e3814332658
Denis <kanukov.denis@gmail.com>
2022-09-28 15:24:16 +0300

    add date.js module and index.js

--- 02_hello/index.js
+++ 02_hello/index.js
@@ -1,1 +1,7 @@
+const currentTime = require("./date");
+
+console.log(
+  `Today is ${currentTime.getFullYear()}-${currentTime.getUTCMonth()}-${currentTime.getDate()}, the current time is ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`
+);
+
 console.log("OK");

3a94898e78f4697d650f910f7acfee4edbf2f2e5
Denis <kanukov.denis@gmail.com>
2022-10-08 20:49:13 +0300

    getting file hash

--- 03_hash/index.js
+++ 03_hash/index.js
@@ -1,1 +1,26 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+if (process.argv.length < 3) {
+  console.log("You dont add any args");
+  process.exit(1);
+}
+
+fs.createReadStream("test-files/cat.jpg")
+  .pipe(crypto.createHash("sha256").setEncoding("hex"))
+  .on("finish", function () {
+    console.log(this.read()); //the hash
+  });
+
+// fs.readFile('test-files/cat.jpg', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     process.exit(0);
+//   }
+//   // console.log(data);
+//   console.log(hash.update(data));
+// })
+
+console.log(crypto.createHash("sha256"));
+
 console.log("OK");
